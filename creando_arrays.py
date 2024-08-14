162c7bedbd12adb4178cf24584d0ff83cd3f1f24
Mauricio <ing.mgonzalez.82@gmail.com>
2024-08-13 19:34:08 -0500

    Funciones de lista con numpy

--- creando_arrays.py
+++ creando_arrays.py
@@ -1,2 +1,20 @@
 import numpy as np 
 
+# lista = list  (range(0,10));
+lista = np.arange(0,10);
+lista = np.arange(0,20);
+lista = np.arange(0,20,2); # step de 2 en 2
+lista = np.zeros(3); # Array de ceros
+lista = np.zeros((10,10)); # Array de ceros
+lista = np.zeros((10,5)); # Array de ceros
+lista = np.linspace(0,10,10); # Array de 5 elementos entre 0 y 10
+lista = np.linspace(0,10,100); # Array de 5 elementos entre 0 y 100
+lista = np.eye(4);
+lista = np.random.rand();
+lista = np.random.rand(4);
+lista = np.random.rand(4,5);
+lista = np.random.randint(4,15);
+lista = np.random.randint(1,100,Z(10, 10));
+
+print (lista)
+
