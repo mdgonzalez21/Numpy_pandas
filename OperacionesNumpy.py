a5400148189221e0709dd13e0629e5c4b0890745
Mauricio <ing.mgonzalez.82@gmail.com>
2024-08-29 18:34:45 -0500

    Operaciones con numpy

---
+++ OperacionesNumpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+lista = [1, 2];
+print(lista);
+print(lista * 2);
+
+arr = np.arange(0,11);
+arr2 = arr.copy();
+
+print(arr);
+print(arr * 2);
+print(arr + 2);
+print(11 / arr);
+
